4d75f29412d2bf446ae9bdcb3173dd434a709db3
khaisendefo <khaisendefo@gmail.com>
2024-07-27 21:11:32 +0300

    Page ready index.html

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+function toggle() {
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.header__burger');
+    const nav = document.querySelector('.header__nav');
+    const navLinks = document.querySelectorAll('.header__link');
+
+    function closeNav() {
+      toggleButton.classList.remove('active');
+      nav.classList.remove('active');
+    }
+
+    toggleButton.addEventListener('click', function () {
+      toggleButton.classList.toggle('active');
+      nav.classList.toggle('active');
+    });
+
+    document.addEventListener('click', function(event) {
+      if (!nav.contains(event.target) && !toggleButton.contains(event.target)) {
+        closeNav();
+      }
+    });
+
+    document.addEventListener('keydown', function(event) {
+      if (event.key === 'Escape' && nav.classList.contains('active')) {
+        closeNav();
+      }
+    });
+
+    navLinks.forEach(function(navLink) {
+      navLink.addEventListener('click', function() {
+        closeNav();
+      });
+    });
+  });
+}
+
+toggle();
